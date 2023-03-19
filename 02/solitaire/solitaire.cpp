02a2e1bf6ec3858b6de31408d069f27ef191c38b
Petr Laštovička <krasik.peta@seznam.cz>
2023-03-19 17:23:46 +0100

    w02(solitaire): First attempt - almost idk

---
+++ 02/solitaire/solitaire.cpp
@@ -0,0 +1,140 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <array>
+#include <queue>
+#include <algorithm>
+
+const int PROBLEM_SIZE = 4;
+const int STEPS_NEEDED = 4;
+
+using Position = std::pair<char, char>;
+using Config = std::array<Position, PROBLEM_SIZE>;
+
+Position operator+(const Position& l, const Position& r) { 
+    return {l.first + r.first, l.second + r.second};                                    
+}
+
+void operator+=(Position& l, const Position& r) { 
+    l.first  += r.first;
+    l.second += r.second;
+}
+
+int toIndex(const Config& config) {
+    return  0
+        + config[0].first  * 1
+        + config[0].second * 8
+        + config[1].first  * 8 * 8
+        + config[1].second * 8 * 8 * 8
+        + config[2].first  * 8 * 8 * 8 * 8
+        + config[2].second * 8 * 8 * 8 * 8 * 8
+        + config[3].first  * 8 * 8 * 8 * 8 * 8 * 8
+        + config[3].second * 8 * 8 * 8 * 8 * 8 * 8 * 8
+        ;
+}
+
+bool readLine(Config& config) {
+    for (int i = 0; i < PROBLEM_SIZE; ++i) {
+        int x, y;
+        if (scanf("%d %d", &x, &y) < 0) {
+            return false;
+        }
+        config[i] = {x - 1, y - 1};
+    }
+
+    return true;
+}
+
+void enqueueWithDelta(const Config& config, const char i, const char dist, std::vector<char>& visited, std::queue<Config>& queue, const Position& delta) {
+    Position newPos = config[i] + delta;
+
+    if (std::any_of(config.begin(), config.end(), [&newPos](const Position& pos){ return pos == newPos; })) {
+        newPos += delta;
+
+        if (std::any_of(config.begin(), config.end(), [&newPos](const Position& pos){ return pos == newPos; })) {
+            return;
+        }
+    }
+
+    if (newPos.first < 0 || newPos.first >= 8 || newPos.second < 0 || newPos.second >= 8) {
+        return;
+    }
+
+    Config newConfig = config;
+    newConfig[i] = newPos;
+    const int nextIndex = toIndex(newConfig);
+    if (visited[nextIndex] != 0) {
+        return;
+    }
+    visited[nextIndex] = dist + 1;
+    queue.emplace(newConfig);
+}
+
+template<bool larger>
+void enqueueNeibours(const Config& config, const char dist, std::vector<char>& visited, std::queue<Config>& queue) {
+    if (dist + (larger ? 1 : 0) > STEPS_NEEDED + 1) return;
+
+    for (int i = 0; i < PROBLEM_SIZE; ++i) {
+        enqueueWithDelta(config, i, dist, visited, queue, { 1,  0});
+        enqueueWithDelta(config, i, dist, visited, queue, {-1,  0});
+        enqueueWithDelta(config, i, dist, visited, queue, { 0,  1});
+        enqueueWithDelta(config, i, dist, visited, queue, { 0, -1});
+    }
+}
+
+bool solve() {
+    Config from, to;
+    if (!readLine(from)) return false;
+    readLine(to);
+
+    std::vector<char> visitedFrom(8 * 8 * 8 * 8 * 8 * 8 * 8 * 8);
+    std::vector<char> visitedTo(8 * 8 * 8 * 8 * 8 * 8 * 8 * 8);
+
+    {
+        std::queue<Config> queue;
+        queue.push(from);
+        visitedFrom[toIndex(from)] = 1;
+
+        while(!queue.empty()) {
+            const Config config = queue.front();
+            queue.pop();
+
+            const int index = toIndex(config);
+            const char dist = visitedFrom[index];
+
+            enqueueNeibours<true>(config, dist, visitedFrom, queue);
+        }
+    }
+
+    {
+        std::queue<Config> queue;
+        queue.push(to);
+        visitedTo[toIndex(to)] = 1;
+
+        while(!queue.empty()) {
+            const Config config = queue.front();
+            queue.pop();
+
+            const int index = toIndex(config);
+            const char dist = visitedTo[index];
+            if (visitedFrom[index] != 0) {
+                printf("YES\n");
+                return true;
+            }
+
+            enqueueNeibours<false>(config, dist, visitedTo, queue);
+        }
+    }
+
+    printf("NO\n");
+    return true;
+}
+
+int main(void) {
+    while(solve());
+    return 0;
+}
+
+
+
